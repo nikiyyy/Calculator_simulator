f90ce66659203d96b665a375ed2c7774446e8236
nikiyyy <wow_niko@protonmail.com>
2021-12-21 13:25:03 +0200

    v2

--- citizen-lc-110n-calculator/src/index.js
+++ citizen-lc-110n-calculator/src/index.js
@@ -14,7 +14,6 @@ var devide_Flag=false;
 var firstNumber=0;
 var secondNumber=0;
 
-console.log("gfddg");
 function clearAllFlags(){
 	nextNumber_Flag=false;
 	plus_Flag=false;
@@ -69,7 +68,6 @@ function devideNumbers(){
 	devide_Flag=true;
 	firstNumber=document.getElementById("output").innerHTML;
 	document.getElementById("output").innerHTML = 0;
-
 }
 
 function multiplyNumbers(){
@@ -125,39 +123,39 @@ function Main(){
         <p>ON</p>
         <table>
           <tr>
-            <td><button id="on_off" type="button" onclick="deleteAll()">C/CE</button></td>
-            <td><button type="button" onclick="tirnOff()">OFF</button></td>
-            <td><button type="button" onclick="squareroot()">√</button></td>
-            <td><button type="button" onclick="">%</button></td>
+            <td><button id="tirnOn" type="button">C/CE</button></td>
+            <td><button id="tirnOff" type="button">OFF</button></td>
+            <td><button id="squareroot" type="button" >√</button></td>
+            <td><button id="percent" type="button" >%</button></td>
           </tr>
           <tr>
-            <td><button type="button" onclick="">MRC</button></td>
-            <td><button type="button" onclick="">M-</button></td>
-            <td><button type="button" onclick="">M+</button></td>
-            <td><button type="button" onclick="devideNumbers()">÷</button></td>
+            <td><button id="mrc" type="button" >MRC</button></td>
+            <td><button id="m" type="button" >M-</button></td>
+            <td><button id="mplus" type="button" >M+</button></td>
+            <td><button id="devideNumbers" type="button">÷</button></td>
           </tr>
           <tr>
-            <td><button type="button" onclick="appendToScreen(7)">7</button></td>
-            <td><button type="button" onclick="appendToScreen(8)">8</button></td>
-            <td><button type="button" onclick="appendToScreen(9)">9</button></td>
-            <td><button type="button" onclick="multiplyNumbers()">X</button></td>
+            <td><button id="aToS7" type="button" >7</button></td>
+            <td><button id="aToS8" type="button" >8</button></td>
+            <td><button id="aToS9" type="button" >9</button></td>
+            <td><button id="multi" type="button" onclick="multiplyNumbers()">X</button></td>
           </tr>
           <tr>
-            <td><button type="button" onclick="appendToScreen(4)">4</button></td>
-            <td><button type="button" onclick="appendToScreen(5)">5</button></td>
-            <td><button type="button" onclick="appendToScreen(6)">6</button></td>
-            <td><button type="button" onclick="subtractNumbers()">-</button></td>
+            <td><button id="aToS4" type="button" >4</button></td>
+            <td><button id="aToS5" type="button" >5</button></td>
+            <td><button id="aToS6" type="button" >6</button></td>
+            <td><button id="subtract" type="button" >-</button></td>
           </tr>
           <tr>
-            <td><button type="button" onclick="appendToScreen(1)">1</button></td>
-            <td><button type="button" onclick="appendToScreen(2)">2</button></td>
-            <td><button type="button" onclick="appendToScreen(3)">3</button></td>
-            <td rowspan="2"><button id="plus" type="button" onclick="addNumbers()">+</button></td>
+            <td><button id="aToS1" type="button"> 1</button></td>
+            <td><button id="aToS2" type="button"> 2</button></td>
+            <td><button id="aToS3" type="button"> 3</button></td>
+            <td rowspan="2"><button id="plus" type="button" >+</button></td>
           </tr>
           <tr>
-            <td><button type="button" onclick="appendToScreen(0)">0</button></td>
-            <td><button type="button" onclick="appendToScreen(.)">.</button></td>
-            <td><button type="button" onclick="calculate()">=</button></td>
+            <td><button id="0" type="button"> 0</button></td>
+            <td><button id="droba" type="button" onclick="appendToScreen(.)">.</button></td>
+            <td><button id="calculate" type="button" >=</button></td>
           </tr>
          </table>
         </div>
@@ -167,3 +165,25 @@ function Main(){
   );
 }
 reactDom.render(<Main />, document.getElementById('root'))
+
+document.getElementById("tirnOn").addEventListener("click", deleteAll );
+document.getElementById("tirnOff").addEventListener("click", tirnOff );
+
+document.getElementById("squareroot").addEventListener("click", squareroot );
+document.getElementById("devideNumbers").addEventListener("click", devideNumbers );
+document.getElementById("multi").addEventListener("click", multiplyNumbers );
+document.getElementById("subtract").addEventListener("click", subtractNumbers );
+document.getElementById("plus").addEventListener("click", addNumbers );
+
+document.getElementById("calculate").addEventListener("click", calculate );
+
+
+document.getElementById("aToS7").addEventListener("click", appendToScreen.bind(null,7) );
+document.getElementById("aToS8").addEventListener("click", appendToScreen.bind(null,8) );
+document.getElementById("aToS9").addEventListener("click", appendToScreen.bind(null,9) );
+document.getElementById("aToS4").addEventListener("click", appendToScreen.bind(null,4) );
+document.getElementById("aToS5").addEventListener("click", appendToScreen.bind(null,5) );
+document.getElementById("aToS6").addEventListener("click", appendToScreen.bind(null,6) );
+document.getElementById("aToS1").addEventListener("click", appendToScreen.bind(null,1) );
+document.getElementById("aToS2").addEventListener("click", appendToScreen.bind(null,2) );
+document.getElementById("aToS3").addEventListener("click", appendToScreen.bind(null,3) );
